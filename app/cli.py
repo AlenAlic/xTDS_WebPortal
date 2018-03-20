265f11e0f4c312c246d633ba9a124856f8ca32e9
AlenAlic <a.alic89@gmail.com>
2018-03-20 15:55:56 +0100

    Webportal template made

--- app/cli.py
+++
@@ -1,6 +0,0 @@
-import os
-import click
-
-
-def register(app):
-    pass
--- app/main/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app.main import bp
-
-
-@bp.route('/')
-@bp.route('/index')
-def index():
-    return render_template('index.html')
