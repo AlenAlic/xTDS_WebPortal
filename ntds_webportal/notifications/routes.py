358f21c7e06fd044afe880f5c4969ac9b89d9993
Tim Broenink <t.g.broenink@utwente.nl>
2018-04-18 11:33:29 +0200

    Initial notification rendering

--- ntds_webportal/notifications/routes.py
+++ ntds_webportal/notifications/routes.py
@@ -2,10 +2,13 @@ from flask import render_template, url_for, redirect, flash
 from flask_login import current_user, login_user, login_required, logout_user
 from ntds_webportal import db
 from ntds_webportal.notifications import bp
-from ntds_webportal.models import User
+from ntds_webportal.models import Notification
 from ntds_webportal.auth.forms import LoginForm, ChangePasswordForm, TreasurerForm
 
 
 @bp.route('/list')
+@login_required
 def list():
-    return render_template('todo.html', title="Notifications")
+    notifications = Notification.query.filter_by(user=current_user, archived=False)\
+        .order_by(Notification.unread.desc()).all()
+    return render_template('notifications/list.html', title="Notifications", notifications=notifications)
