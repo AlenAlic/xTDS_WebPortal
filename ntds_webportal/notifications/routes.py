f4d0f89b44bd0dabf7500e0ffaa90e92b31c729e
Tim Broenink <t.g.broenink@utwente.nl>
2018-05-01 19:43:56 +0200

    Notification styling with sender and timestamp

--- ntds_webportal/notifications/routes.py
+++ ntds_webportal/notifications/routes.py
@@ -15,8 +15,8 @@ def list():
         notifications_query = Notification.query.filter_by(user=current_user)
     else:
         notifications_query = Notification.query.filter_by(user=current_user, archived=False)
-    notifications = notifications_query.order_by(Notification.notification_id.desc())\
-                                       .order_by(Notification.unread.desc()).all()
+    notifications = notifications_query.order_by(Notification.notification_id.desc()) \
+        .order_by(Notification.unread.desc()).all()
     return render_template('notifications/list.html', title="Notifications", notifications=notifications,
                            show_archived=show_archived)
 
@@ -92,7 +92,7 @@ def goto(notification):
 def create():
     print("Processing Form")
     form = NotificationForm()
-    choices = [('tc','All Teamcaptains'),('tr','All Treasurers')]
+    choices = [('tc', 'All Teamcaptains'), ('tr', 'All Treasurers')]
     for user in User.query.all():
         choices.append(('{}'.format(user.user_id), user.username))
     form.recipients.choices = choices
@@ -101,16 +101,16 @@ def create():
             if recipient.isdigit():
                 u = User.query.filter_by(user_id=recipient).first()
                 n = Notification(title=form.title.data, text=form.body.data,
-                                 user=u)
+                                 user=u, sender=current_user)
                 db.session.add(n)
             else:
-                if recipient=='tc':
-                    users= User.query.filter_by(access=data.ACCESS['team_captain']).all()
-                if recipient=='tr':
-                    users= User.query.filter_by(access=data.ACCESS['treasurer']).all()
+                if recipient == 'tc':
+                    users = User.query.filter_by(access=data.ACCESS['team_captain']).all()
+                if recipient == 'tr':
+                    users = User.query.filter_by(access=data.ACCESS['treasurer']).all()
                 for u in users:
                     n = Notification(title=form.title.data, text=form.body.data,
-                                     user=u)
+                                     user=u, sender=current_user)
                     db.session.add(n)
 
         db.session.commit()
