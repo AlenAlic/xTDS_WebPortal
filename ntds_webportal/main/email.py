0f567a09bd736e17d1c6b71a7780666f91b8c997
AlenAlic <a.alic89@gmail.com>
2018-07-02 15:23:45 +0200

    send email selection after raffle fix

--- ntds_webportal/main/email.py
+++ ntds_webportal/main/email.py
@@ -1,10 +1,10 @@
-from flask import render_template
+from flask import render_template, current_app
 from ntds_webportal.email import send_email
 
 
 def send_new_messages_email(sender, recipient):
     send_email('New messages - xTDS WebPortal',
-               sender=sender.email,
+               sender=current_app.config['ADMINS'][0],
                recipients=[recipient.email],
                text_body=render_template('email/new_message.txt', sender=sender, recipient=recipient),
                html_body=render_template('email/new_message.html', sender=sender, recipient=recipient))
