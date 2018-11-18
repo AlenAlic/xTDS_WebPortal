38c207da79e062a52bc148c0f4a43e7d6474aa92
AlenAlic <a.alic89@gmail.com>
2018-11-18 17:13:17 +0100

    notifications fix

--- ntds_webportal/notifications/forms.py
+++ ntds_webportal/notifications/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 class NotificationForm(FlaskForm):
     title = StringField(label="Title", validators=[DataRequired()])
     body = TextAreaField(label="Message content", validators=[DataRequired()],
-                         render_kw={"style": "resize:none", "rows": "4", "maxlength": "512"})
+                         render_kw={"style": "resize:none", "rows": "12", "maxlength": "512"})
     recipients = SelectMultipleField(label="Recipients", validators=[DataRequired()])
     submit = SubmitField('Send message')
