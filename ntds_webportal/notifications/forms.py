4379bd89aa634d08065ffd72ece31102d9f4bc7a
AlenAlic <a.alic89@gmail.com>
2018-11-22 18:02:08 +0100

    no message limit on form

--- ntds_webportal/notifications/forms.py
+++ ntds_webportal/notifications/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 class NotificationForm(FlaskForm):
     title = StringField(label="Title", validators=[DataRequired()])
     body = TextAreaField(label="Message content", validators=[DataRequired()],
-                         render_kw={"style": "resize:none", "rows": "20", "maxlength": "4096"})
+                         render_kw={"style": "resize:none", "rows": "20"})
     recipients = SelectMultipleField(label="Recipients", validators=[DataRequired()],
                                      render_kw={'data-role': 'select2'})
     submit = SubmitField('Send message')
