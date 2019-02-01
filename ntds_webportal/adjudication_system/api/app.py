0975ae99ee06b00139a12439915b1f085757d43d
AlenAlic <a.alic89@gmail.com>
2019-02-01 11:42:03 +0100

    app heat list update

--- ntds_webportal/adjudication_system/api/app.py
+++ ntds_webportal/adjudication_system/api/app.py
@@ -9,13 +9,14 @@ def app_competition_list():
     competitions = Competition.query.join(DancingClass).filter(DancingClass.name != TEST)\
         .order_by(Competition.when).all()
     if len(competitions) > 0:
-        return jsonify({i: {"id": c.competition_id, "name": c.name()} for i, c in enumerate(competitions)})
+        return jsonify({i: {"id": c.competition_id, "name": c.name(), 'date': c.when}
+                        for i, c in enumerate(competitions)})
     return Response(status=204)
 
 
 @bp.route('/app/competition_heat_list/<int:competition_id>', methods=["GET"])
 def app_competition_heat_list(competition_id):
-    competition = Competition.query.get(competition_id)
+    competition = Competition.query.get_or_404(competition_id)
     if competition is not None:
         if competition.dancing_class.name != TEST and competition.heat_list is not None:
             return competition.heat_list
