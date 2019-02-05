bb47a2b6dcdfe4bee223c41484d0daac425bc663
AlenAlic <a.alic89@gmail.com>
2019-02-05 02:12:01 +0100

    api CORS fix

--- ntds_webportal/adjudication_system/api/__init__.py
+++ ntds_webportal/adjudication_system/api/__init__.py
@@ -6,3 +6,10 @@ from ntds_webportal.adjudication_system.api import adjudication
 from ntds_webportal.adjudication_system.api import tournament_office
 from ntds_webportal.adjudication_system.api import floor_manager
 from ntds_webportal.adjudication_system.api import app
+
+
+@bp.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
--- ntds_webportal/auth/routes.py
+++ ntds_webportal/auth/routes.py
@@ -14,7 +14,7 @@ def reset_password_request():
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user:
+        if user is not None:
             send_password_reset_email(user)
         flash('Check your email for the instructions to reset your password.')
         return redirect(url_for('main.index'))
